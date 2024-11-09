4227db2d60227c64beadd66a138e4dcef4d41802
Daniel Brathagen <dbrathagen@gmail.com>
2024-11-09 11:37:26 +0000

    Arm the create lobby button and create api types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  function onCreate() {
+    fetch("/api/create", {
+      method: "POST"
+    }).then((res) =>
+      res.json()
+    ).then((data: CreateLobbyResponse) => {
+      redirect(data.url)
+    })
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-background text-foreground gap-10">
       <h1 className="text-4xl font-bold">RadioGuessr</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button onClick={onCreate} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Create lobby
       </button>
     </div>
